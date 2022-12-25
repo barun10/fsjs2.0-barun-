befa9e121a49c47fca903a04fb5fc91f1d27c327
Barunabha Poddar <barun1996poddar@gmail.com>
2022-12-25 23:43:19 +0530

    index2.js file edited(part-1)

---
+++ My practices/my javascript/index2.js
@@ -0,0 +1,38 @@
+// console.log("hello world");
+
+//creating an array
+// let arr = new Array()
+// let arr2 = []
+
+// let fruits = ["apple", "banana", "cherry"];
+
+// console.log(fruits); 
+// ["apple", "banana", "cherry"]
+// console.log(fruits[0]); 
+// "apple"
+// console.log(fruits[1]); 
+// "banana"
+// console.log(fruits[2]); 
+// "cherry"
+
+//Replacing array elements
+// fruits[2] = "pear";
+// 
+// console.log(fruits);
+// 
+// adding new element
+// fruits[3] = "mango";
+// 
+// 
+// console.log(fruits);
+
+//storing different types of elements
+
+// let diff = ["apple", 1, "🥑", "banana", true, "cherry"];
+
+// console.log(diff);
+
+// console.log(diff[4]);
+
+//checking the type of element
+// console.log(typeof diff[4]);
