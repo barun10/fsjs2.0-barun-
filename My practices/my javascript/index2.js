6e6227c4689395b74911acf746f02a6cb29480ce
Barunabha Poddar <barun1996poddar@gmail.com>
2022-12-27 23:46:04 +0530

    index2.js file edited(part-3)

--- My practices/my javascript/index2.js
+++ My practices/my javascript/index2.js
@@ -69,3 +69,34 @@
 // console.log(it.next().value)
 // console.log(it.next().value)
 // console.log(it.next().value)
+
+// every method
+
+// let num = [1,2,3,4,5,6,7,8,9,10]
+// console.log(num.every((a)=> (a % 2 == 0)));
+
+//fill and filter method
+
+// let num = [1,2,3,4,5,6,7,8,9,10]
+// console.log(num.fill(2, 3, 5))
+
+// let num = [1,2,3,4,5,6,7,8,9,10]
+// console.log(num.filter(num => num % 2 == 1))
+
+//find and findlast method
+
+// let num = [1,2,3,4,6,7,8,9]
+// console.log(num.find(num => num > 4))
+// console.log(num.findLast(num => num > 4))
+
+//findindex and findlastindex method
+
+// let num = [1,2,3,4,5,6,7,8,9]
+// console.log(num.findIndex(num => num > 4))
+// console.log(num.findLastIndex(num => num > 5))
+
+//flat method
+
+// let num = [1,2,3,[4,5],[6,7,[[8,9]]]]
+
+// console.log(num.flat());
