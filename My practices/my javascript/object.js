9cff66074cba088b24493197cb674480e31c6c73
Barunabha Poddar <barun1996poddar@gmail.com>
2023-01-06 11:23:09 +0530

    object.js part-1 added

---
+++ My practices/my javascript/object.js
@@ -0,0 +1,58 @@
+//assign method
+// const obj1 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+// const obj2 = {
+//   a: "A",
+//   b: "B",
+//   c: "C"
+// };
+
+// const rt = Object.assign(obj1, obj2);
+// console.log(obj1)
+// // console.log(obj2)
+// console.log(rt === obj1)
+//assign method cloning
+
+// const alpha = {
+//   wolf: '🐺',
+//   lion: '🦁'
+// };
+
+// const animal = Object.assign({}, alpha);
+// console.log(animal)
+// console.log(animal === alpha)
+
+// const o1 = {a:1}
+// const o2 = {b:2}
+// const o3 = {c:3}
+
+// const obj = Object.assign(o1, o2, o3);
+// console.log(obj)
+// console.log(o1) 
+
+// const alpha = {
+//   wolf: '🐺',
+//   lion: '🦁',
+//   pig: '🐖'
+// }
+
+// const beta = {
+//   wolf: '🐄',
+//   lion: '🐯',
+// }
+// const gama = {
+//   pig: "🐍"
+// }
+
+// const animal = Object.assign({}, alpha, beta, gama);
+// console.log(animal) 
+// const obj = Object.create({ foo: 1 }, { // foo is on obj's prototype chain.
+//   bar: {value: 2},
+//   baz: {value: 3,enumerable: true}
+// });
+// const copy = Object.assign({}, obj);
+// console.log(copy)
