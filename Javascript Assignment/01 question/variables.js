01e934a4ea15220f7ddd2acbc73af40463cc4378
Barunabha Poddar <barun1996poddar@gmail.com>
2023-01-27 08:12:45 +0530

    Javascript Assignment q1 added

---
+++ Javascript Assignment/01 question/variables.js
@@ -0,0 +1,15 @@
+let my_string = "hello"
+console.log(my_string)
+console.log(typeof my_string)
+
+let my_bool = false
+console.log(my_bool)
+console.log(typeof my_bool)
+
+let my_undef
+console.log(my_undef)
+console.log(typeof my_undef)
+
+let my_null = null
+console.log(my_null)
+console.log(typeof my_null)
