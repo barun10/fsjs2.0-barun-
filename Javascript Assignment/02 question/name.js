d91b0d11db5df16305063dcde3e9ba89e18832bf
Barunabha Poddar <barun1996poddar@gmail.com>
2023-01-28 08:12:45 +0530

    Javascript Assignment q2 added

---
+++ Javascript Assignment/02 question/name.js
@@ -0,0 +1,7 @@
+let first_name = "Barunabha"
+let last_name = "Poddar"
+let marital_status = "unmarried"
+let country = "India"
+let age = 26
+
+console.log(`Hello, my name is ${first_name} ${last_name}. I am ${marital_status}. I live in ${country} and my age is ` + age)
