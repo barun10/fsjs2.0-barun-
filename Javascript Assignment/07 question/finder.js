2a08527e8428bcfbde41e274e1ccd8e6ff8dd885
Barunabha Poddar <barun1996poddar@gmail.com>
2023-02-02 21:12:45 +0530

    Javascript Assignment q7 added

---
+++ Javascript Assignment/07 question/finder.js
@@ -0,0 +1,4 @@
+let words = ["hello", "I", "am", "a", "techear", "at", "pw skills", "and", "happy", "at", "pw skills", "thank", "you"]
+
+console.log("The first index is " + words.lastIndexOf("pw skills",-4))
+console.log("The last index is " + words.lastIndexOf("pw skills"))
