b7af6c69034e4d076afa4febac9f841e7722cd6f
Barunabha Poddar <barun1996poddar@gmail.com>
2023-02-05 10:12:45 +0530

    Javascript Assignment q9 added

---
+++ Javascript Assignment/09 question/bool.js
@@ -0,0 +1,10 @@
+let a = 2
+let b = "hello"
+let c = 3
+console.log(a == 2)
+console.log(typeof b === 'string')
+console.log(Math.floor(c/a) == 1)
+
+console.log(a == 1)
+console.log(typeof a === 'string')
+console.log(Math.ceil(c/a) == 1)
