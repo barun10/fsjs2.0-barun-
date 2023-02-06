93028e23767efcdcfaaa6bb00b919fc4f35f54ef
Barunabha Poddar <barun1996poddar@gmail.com>
2023-02-06 16:12:45 +0530

    Javascript Assignment q10 added

---
+++ Javascript Assignment/10 question/detect.js
@@ -0,0 +1,56 @@
+// 4>3 
+// Expected value:- true
+console.log(4>3) 
+//Actual value:- true
+
+// 4 >= 3
+//Expected value:- true
+console.log(4>= 3)
+// Actual value:- true
+
+//4 < 3
+//Expected value:- false
+console.log(4 < 3)
+//Actual value:- false
+
+// 4 <= 3
+//Expected value:- false
+console.log(4<=3)
+//Actual value:- false
+
+//4 == 4
+// Expected value:- true
+console.log(4 == 4)
+// Actual value:- true
+
+//4 === 4
+//Expected value:- true
+console.log(4 === 4)
+//Actual value:- true
+
+// 4 != 4
+//Expected value:- false
+console.log(4 != 4)
+//Actual value:- false
+
+//4 !== 4
+//Expected value:- false
+console.log(4 !== 4)
+//Actual value:- false
+
+//4 != '4'
+//Expected value:- false
+console.log(4 != '4')
+//Actual value:- false
+
+// 4 == '4'
+//Expected value:- true
+console.log(4 == '4')
+// Actual value:- true
+
+// 4 === '4'
+//Expected value:- false
+console.log(4 === '4')
+//Actual value:- false
+
+console.log("python".length != "jargon".length)
