854bc288fd76d8031ce3c738a877a3ec96187991
Barunabha Poddar <barun1996poddar@gmail.com>
2023-02-23 10:10:00 +0530

    Javascript Assignment q20 added

---
+++ Javascript Assignment/20 question/shopping.js
@@ -0,0 +1,9 @@
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift("Meat")
+console.log(shoppingCart)
+
+shoppingCart.push("Sugar")
+console.log(shoppingCart)
+
+shoppingCart.splice(4,1)
+console.log(shoppingCart)
