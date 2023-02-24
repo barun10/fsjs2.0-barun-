3455720757c3e595aa4e6810310df8baa00cb942
Barunabha Poddar <barun1996poddar@gmail.com>
2023-02-24 08:10:00 +0530

    Javascript Assignment q21 added

---
+++ Javascript Assignment/21 question/countries2.js
@@ -0,0 +1,8 @@
+let countries = ["India", "Australia", "United States of America", "China", "Japan"]
+
+if (countries.includes("ethiopia"))
+  console.log("ETHIOPIA")
+else
+  countries.push("Ethiopia")
+  console.log("Ethiopia is added")
+  console.log(countries)
